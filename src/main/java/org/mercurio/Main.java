06e5c7c1d276c9ccd0db01f735db139d6ffdcdab
albvalort <albvalort@outlook.es>
2025-10-14 23:12:56 +0200

    Gradle configs and javafx

--- src/main/java/org/mercurio/Main.java
+++ src/main/java/org/mercurio/Main.java
@@ -1,10 +1,37 @@
 package org.mercurio;
 
-public class Main {
-    static void main() {
-        IO.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            IO.println("i = " + i);
-        }
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Panel panel = new Panel("This is the title");
+        panel.getStyleClass().add("panel-primary");                            //(2)
+        BorderPane content = new BorderPane();
+        content.setPadding(new Insets(20));
+        Button button = new Button("Hello BootstrapFX");
+        button.getStyleClass().setAll("btn","btn-danger");                     //(2)
+        content.setCenter(button);
+        panel.setBody(content);
+
+        Scene scene = new Scene(panel);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());       //(3)
+
+        primaryStage.setTitle("BootstrapFX");
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    static void main(String[] args) {
+        launch();
     }
 }

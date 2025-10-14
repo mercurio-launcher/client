19feaf7368674486c62988c4eeac26b18201c0c6
albvalort <albvalort@outlook.es>
2025-10-14 21:46:44 +0200

    First intro for the project

---
+++ src/main/java/org/mercurio/Main.java
@@ -0,0 +1,10 @@
+package org.mercurio;
+
+public class Main {
+    static void main() {
+        IO.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}

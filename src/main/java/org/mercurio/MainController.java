671bb93553b1a8e2a4c24b32902574dd5feb5a02
Miguel Angel Ortiz Requena <MAORTREQ@upv.edu.es>
2025-10-16 18:42:32 +0200

    Third GUI, new fxml and buttons added. Still no boostrap.

--- src/main/java/org/mercurio/MainController.java
+++ src/main/java/org/mercurio/MainController.java
@@ -20,6 +20,12 @@ public class MainController {
     @FXML private ToggleButton btnHome;
     @FXML private ToggleButton btnSettings;
     @FXML private ToggleButton btnReports;
+    @FXML private ToggleButton btnProfile;
+    @FXML private ToggleButton btnAnalytics;
+    @FXML private ToggleButton btnTasks;
+    @FXML private ToggleButton btnHelp;
+
+
 
     private final ToggleGroup navGroup = new ToggleGroup();
     private final Map<String, Parent> cache = new HashMap<>();
@@ -30,6 +36,10 @@ public class MainController {
         btnHome.setToggleGroup(navGroup);
         btnSettings.setToggleGroup(navGroup);
         btnReports.setToggleGroup(navGroup);
+        btnProfile.setToggleGroup(navGroup);
+        btnAnalytics.setToggleGroup(navGroup);
+        btnTasks.setToggleGroup(navGroup);
+        btnHelp.setToggleGroup(navGroup);
 
         // --- NUEVO ---
         // Añadir o quitar la clase "active" para estilo visual (sin usar :selected)
@@ -45,8 +55,12 @@ public class MainController {
 
         // Eventos de navegación
         btnHome.setOnAction(e -> navigateTo("Home", "/org/mercurio/views/HomeView.fxml"));
-        btnSettings.setOnAction(e -> navigateTo("Ajustes", "/org/mercurio/views/SettingsView.fxml"));
-        btnReports.setOnAction(e -> navigateTo("Informes", "/org/mercurio/views/ReportsView.fxml"));
+        btnSettings.setOnAction(e -> navigateTo("Settings", "/org/mercurio/views/SettingsView.fxml"));
+        btnReports.setOnAction(e -> navigateTo("Reports", "/org/mercurio/views/ReportsView.fxml"));
+        btnProfile.setOnAction(e -> navigateTo("Profile","/org/mercurio/views/ProfileView.fxml"));
+        btnAnalytics.setOnAction(e -> navigateTo("Analalytics","/org/mercurio/views/AnalyticsView.fxml"));
+        btnTasks.setOnAction(e -> navigateTo("Tasks","/org/mercurio/views/TasksView.fxml"));
+        btnHelp.setOnAction(e -> navigateTo("Help","/org/mercurio/views/HelpView.fxml"));
 
         // Carga inicial
         btnHome.setSelected(true);
